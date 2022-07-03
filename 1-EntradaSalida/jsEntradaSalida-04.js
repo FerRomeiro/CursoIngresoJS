1b8ce1f5dd1d6002adb5daf417e630f74abefa94
FerRomeiro <02FernandoRomero@gmail.com>
2022-07-03 19:59:05 -0300

    noveno

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -6,6 +6,8 @@ function mostrar()
 {
 	let nombre;
 	nombre = prompt('ingrese nombre');
-	document.getElementById("txtIdNombre").value = nombre;
+	document.getElementById("txtIdNombre").value = nombre; 
+	//EL VALOR DE LA CAJA DE TEXTO txtIdNombre(value) ES LO QUE VOY A TENER EN ESA VARIABLE
+	// QUE ESE VALOR SERIA LO QUE PONGA EN PROMPT QUE PERTENECE A LA VARIABLE nombre
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -10,4 +10,6 @@ function mostrar()
 	//alert(" usted se llama " + nombre + " y tiene " + edad + " años");
     alert(`Usted se llama ${nombre} y se llama ${edad}`); // alt + 96 ``````
 } 
-
+// alert(nombre) MUESTRA EL VALOR DE LA VARIABLE
+// alert("hola") MUESTRA EL TEXTO QUE ESCRIBI 
+// alert(`usted se llama ${nombre} y se llama ${edad}`); COMBINA AMBAS FORMAS 
