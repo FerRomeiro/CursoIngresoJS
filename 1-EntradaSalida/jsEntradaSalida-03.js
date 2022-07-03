bf75298311e329b01e5ec87035004f69f9cd7ffa
FerRomeiro <02FernandoRomero@gmail.com>
2022-07-03 19:08:24 -0300

    octavo

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -5,10 +5,11 @@ function mostrar()
 {
 	let nombreIngresado;
 	nombreIngresado=txtIdNombre.value;
-	alert(nombreIngresado);
-
+	alert(nombreIngresado); 
+	txtIdNombre.value = "";
 }
 /* let nombre;
    nombre = document.getelementById("txtIdNombre").value;
    alert(nombre); */
+// document.getElementById("txtIdNombre").value = ""; PARA QUE LA CAJA DE TEXTO APAREZCA EN BLANCO 
 
