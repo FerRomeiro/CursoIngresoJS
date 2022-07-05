7834231374b8a815dff899c6fd914ff1a287c1aa
FerRomeiro <02FernandoRomero@gmail.com>
2022-07-04 21:14:36 -0300

    onceavo

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	alert("ok");
-}
+  let num1;
+  let aumento;
+  let num2;
+  
+  num1 = parseInt(document.getElementById('txtIdSueldo').value);
+  aumento = num1 * 10 / 100;
+  num2 = num1 + aumento;
+  document.getElementById('txtIdResultado').value = num2;
+  
+} 
