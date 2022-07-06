680240370b066a205104215587575b448453e808
FerRomeiro <02FernandoRomero@gmail.com>
2022-07-05 22:27:49 -0300

    bis

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,7 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+  /*
   let num1;
   let aumento;
   let num2;
@@ -13,5 +14,16 @@ function mostrarAumento()
   aumento = num1 * 10 / 100;
   num2 = num1 + aumento;
   document.getElementById('txtIdResultado').value = num2;
+  */
+  let num1; //sueldo
+  let num2; // resultado
+  let num3; // porcentaje
+
+  num1 = parseInt(document.getElementById('txtIdSueldo').value);
+  num3= prompt('ingrese porcentaje');
+  num2 = num1 * num3 / 100;
   
+  document.getElementById('txtIdResultado').value = num2;
+  
+
 } 
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,14 +5,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-  let sueldo;
+  /*let sueldo;
   let descuento;
   let resultado;
   sueldo = parseInt(document.getElementById('txtIdImporte').value);
   descuento = sueldo * 25 / 100;
   resultado = sueldo - descuento;
   document.getElementById('txtIdResultado').value = resultado;
+*/
+  let num1; // sueldo
+  let num2; // descuento
+  let num3; // resultado
 
+  num1 = parseInt(document.getElementById('txtIdImporte').value);
+  num2 = prompt('ingrese el valor del descuento');
+  num3 = num1 * num2/100;
+  document.getElementById('txtIdResultado').value = num3;
 
 }
 
