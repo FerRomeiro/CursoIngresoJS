7c05a7a3a610e4cd69d0c04551aec3bf7a30ed2c
FerRomeiro <02FernandoRomero@gmail.com>
2022-07-09 22:29:39 -0300

    9/7/22

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -5,27 +5,80 @@ function mostrar()
 {
 	let estacion;
 	let destino;
-	let precioViaje = 15000;
+	const precioViaje = 15000;
 	let precioFinal;
 	let precioFinalPorcentaje;
 	
 	estacion = document.getElementById('txtIdEstacion').value;
 	destino = document.getElementById('txtIdDestino').value;
-    precioViaje = parseInt(precioViaje);
+    
+	switch(estacion){
+		case "Invierno":
+			if( destino == "Bariloche" ){
+                precioFinal = (precioViaje*20)/100;
+				precioFinalPorcentaje = precioViaje + precioFinal;
+				alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+            break;
+			}
+		case "Invierno":
+			if( destino == "Cataratas" || destino == "Cordoba"){
+				precioFinal = (precioViaje*10)/100;
+				precioFinalPorcentaje = precioViaje - precioFinal;
+				alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+			break;
+			}
+	    case "Verano":
+			if( destino == "Bariloche"){
+                precioFinal = (precioViaje*20)/100;
+				precioFinalPorcentaje = precioViaje - precioFinal;
+				alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+			break;
+			}
+		case "Verano":
+			if( destino == "Cataratas" || destino == "Cordoba"){
+				precioFinal = (precioViaje*10)/100;
+				precioFinalPorcentaje = precioFinal + precioViaje;
+				alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+			break;
+			}
+		case "Verano":
+		    if( destino == "Mar del plata"){
+			precioFinal = (precioViaje*20)/100;
+			precioFinalPorcentaje = precioFinal+precioViaje;
+			alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+			break;
+		    }
+	
+		case "Otoño":
+		case "Primavera":
+			if( destino == "Cataratas" ){
+            precioFinal = (precioViaje*10)/100;
+			precioFinalPorcentaje = precioFinal + precioViaje;
+			alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+		    }
+			else if( destino == "Mar del plata" ){
+			precioFinal = (precioViaje*10)/100;
+			precioFinalPorcentaje = precioFinal + precioViaje;
+			alert('El precio del viaje es ' + precioFinalPorcentaje + '$');
+			}
+            	
+	}
 
+}  
+
+
+
+
+
+
+
+	/*
 	if( destino == "Bariloche" && estacion == "Invierno" ){
         precioFinal = (precioViaje * 20) / 100;
 			precioFinalPorcentaje = precioFinal + precioViaje;
 			alert('Su precio es ' + precioFinalPorcentaje + '$');  
 	}
-    switch( estacion ){
-            case "Invierno":
-			    if( destino == "Cordoba" && destino == "Cataratas")
-				precioFinal = (precioViaje * 10) / 100;
-				precioFinalPorcentaje = precioViaje - precioFinal;
-				alert('Su precio es ' + precioFinalPorcentaje + '$'); 
-					
-	}
+    */
 /*
 	switch( estacion ){
            case "Invierno":
