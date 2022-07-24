532ca43a89ef7a48050ca594a9f19f09f7ae8f0e
FerRomeiro <02FernandoRomero@gmail.com>
2022-07-24 01:10:14 -0300

    24/7/22

--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -9,32 +9,60 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+	/*
  	let edadIngresada;
 	let respuesta = 'y';
 	let sexo;
 	let estadoCivil;
+	let sueldoBruto;
+	let numeroLegajo;
+	let nacionalidad;
 
 	while(respuesta == 'y'){
           edadIngresada = prompt('ingrese edad entre 18 y 90 años inclusive');
 		  edadIngresada = parseInt(edadIngresada);
-
-		  while(edadIngresada < 17 || edadIngresada > 90){
+		  // EDAD				
+		  while(edadIngresada < 18 || edadIngresada > 90){
                 edadIngresada = prompt('ERROR, ingrese edad nuevamente'); // se tiene q parsear nuevamente el error recordar
 				edadIngresada = parseInt(edadIngresada);
 		  }
-
+		  //SEXO
 		  sexo = prompt('ingrese sexo m/f');
-
+		  
 		  while(sexo != "f" && sexo != "m"){
 			sexo = prompt('INGRESE SEXO VALIDO');
 	      }
-
+		  //ESTADO CIVIL
 		  estadoCivil = prompt('Ingrese estado civil (soltero, casado, divorciado y viudo');
 
 		  while(estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil && "divorciado" && estadoCivil != "viudo" ){
                 estadoCivil = prompt('Ingrese un estado civil valido');
 		  }
+		  ///SUELDO BRUTO
+		  sueldoBruto = prompt('Ingrese el sueldo correspondiente no menor a 8000');
 
+		  while(sueldoBruto<8000){
+				sueldoBruto = prompt('ERROR, INGRESE UN VALOR NO MENOR A 8000');
+				sueldoBruto = parseInt(sueldoBruto);
+		  }
+		  // numero de legajo
+		  numeroLegajo = prompt('INGRESE NUMERO DE LEGAJO ENTRE 1000 Y 9999');
+
+		  while(numeroLegajo<1000 || numeroLegajo>9999){
+			    numeroLegajo = prompt('NUMERO DE LEGAJO INVALIDO, INGRESE NUEVAMENTE');
+				numeroLegajo = parseInt(numeroLegajo);
+		  }
+		  //NACIONALIDAD	
+		  nacionalidad = prompt('INGRESE NACIONALIDAD CORRESPONDIENTE');
+
+		  while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N"){
+			    nacionalidad = prompt('ERROR, INGRESE NACIONALIDAD CORRESPONDIENTE');
+				
+		  }
+
+		  document.getElementById('txtIdNacionalidad').value = nacionalidad;
+		  document.getElementById('txtIdLegajo').value = numeroLegajo;
+		  document.getElementById('txtIdSueldo').value = sueldoBruto;
 		  document.getElementById('txtIdEstadoCivil').value = estadoCivil;
 		  document.getElementById('txtIdSexo').value = sexo;
 		  document.getElementById('txtIdEdad').value = edadIngresada;
@@ -44,3 +72,151 @@ function ComenzarIngreso ()
 
 
 }
+
+
+/*
+TP 12 BIS: Teniendo en cuenta lo realizado en el TP 12, 
+ahora se solicita el ingreso de 10 personas para el censo realizando 
+las validaciones necesarias. Ademas se debe informar el promedio de edades 
+de las personas de sexo femenino.
+*/
+
+	let edadIngresada;	
+	let sexo;
+	let estadoCivil;
+	let sueldoBruto;
+	let numeroLegajo;
+	let nacionalidad;
+	let personas = 0;
+	let sexoF = 0;
+	let edadTotalF = 0;
+	let promedioF;
+
+	while(personas<10){
+
+		edadIngresada = prompt('INGRESE EDAD DE 18 A 90 AÑOS');
+		edadIngresada = parseInt(edadIngresada);
+		while(edadIngresada<18 || edadIngresada>90){
+              edadIngresada=prompt('ERROR, INGRESE UNA EDAD CORRECTA');
+			  edadIngresada=parseInt(edadIngresada);
+		}
+		sexo = prompt('INGRESA UN SEXO f/m')
+		while(sexo != "f" && sexo != "m"){
+              sexo = prompt('ERROR, INGRESE SEXO CORRECTO');			  
+		}
+		
+		estadoCivil = prompt('INGRESE ESTADO CIVIL')
+		while(estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo" && estadoCivil != "divorciado"){
+			  estadoCivil = prompt('INGRESE ESTADO CIVIL CORRECTO');
+		}
+		sueldoBruto = prompt('INGRESE SUELDO BRUTO NO MENOR A 8000')
+		sueldoBruto = parseInt(sueldoBruto);
+		while(sueldoBruto<8000){
+              sueldoBruto = prompt('INGRESE SUELDO BRUTO MENOR A 8000');
+		}
+		numeroLegajo = prompt('INGRESE NUMERO DE LEGAJO');
+		numeroLegajo = parseInt(numeroLegajo);
+		while(numeroLegajo<1000 || numeroLegajo>9999){
+              numeroLegajo=prompt('INGRESE NUMERO DE LEGAJO CORRECTO');
+		}
+		nacionalidad = prompt('INGRESE NACIONALIDAD');
+		while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N"){
+			  nacionalidad=prompt('INGRESE NACIONALIDAD CORRECTA A, E O N')
+		}
+		
+		//promedio de edad se sexo F
+		if(sexo == "f"){
+           sexoF = sexoF + 1
+		   edadTotalF = edadTotalF + edadIngresada;
+		   promedioF = edadTotalF/sexoF;
+		}
+
+		  
+          personas = personas + 1;
+		  
+		  
+
+	}
+		  document.getElementById('txtIdNacionalidad').value = nacionalidad;
+		  document.getElementById('txtIdLegajo').value = numeroLegajo;
+		  document.getElementById('txtIdSueldo').value = sueldoBruto;
+		  document.getElementById('txtIdEstadoCivil').value = estadoCivil;	
+		  document.getElementById('txtIdEdad').value = edadIngresada;
+		  document.getElementById('txtIdSexo').value = sexo;
+		  
+
+	alert('promedio de edades femeninas es ' + promedioF);
+    
+
+
+		  /*
+		  //ESTADO CIVIL
+		  estadoCivil = prompt('Ingrese estado civil (soltero, casado, divorciado y viudo');
+
+		  while(estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil && "divorciado" && estadoCivil != "viudo" ){
+                estadoCivil = prompt('Ingrese un estado civil valido');
+		  }
+		  ///SUELDO BRUTO
+		  sueldoBruto = prompt('Ingrese el sueldo correspondiente no menor a 8000');
+
+		  while(sueldoBruto<8000){
+				sueldoBruto = prompt('ERROR, INGRESE UN VALOR NO MENOR A 8000');
+				sueldoBruto = parseInt(sueldoBruto);
+		  }
+		  // numero de legajo
+		  numeroLegajo = prompt('INGRESE NUMERO DE LEGAJO ENTRE 1000 Y 9999');
+
+		  while(numeroLegajo<1000 || numeroLegajo>9999){
+			    numeroLegajo = prompt('NUMERO DE LEGAJO INVALIDO, INGRESE NUEVAMENTE');
+				numeroLegajo = parseInt(numeroLegajo);
+		  }
+		  //NACIONALIDAD	
+		  nacionalidad = prompt('INGRESE NACIONALIDAD CORRESPONDIENTE');
+
+		  while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N"){
+			    nacionalidad = prompt('ERROR, INGRESE NACIONALIDAD CORRESPONDIENTE');
+				
+		  }
+
+		
+
+		  //respuesta = prompt('quiere ingresar otro dato y/n');
+
+
+		contador = contador + 1;
+		}
+
+		
+
+
+		document.getElementById('txtIdNacionalidad').value = nacionalidad;
+		document.getElementById('txtIdLegajo').value = numeroLegajo;
+		document.getElementById('txtIdSueldo').value = sueldoBruto;
+		document.getElementById('txtIdEstadoCivil').value = estadoCivil;
+		document.getElementById('txtIdSexo').value = sexo;
+		document.getElementById('txtIdEdad').value = edadIngresada;
+
+
+		
+
+		  
+		contador = alert('LLEGO AL LIMITE DE PERSONAS');
+
+		
+
+
+
+	
+	
+
+
+}
+*/
+}
+
+
+
+
+  
+
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,83 @@
+/*
+ ALUMNO: FERNANDO ROMERO MONTERO
+ DIV: "J"
+*/
 function mostrar()
 {
+    let notas;
+    let sexo;
+    let contador = 0;
+    let notasTotales = 0;
+    let notaMinima;
+    let bandera = 0;
+    let sexoPersona;
+    let cantidadVarones = 0;
+
+    
+    while(contador < 5){
+    contador = contador + 1;
+
+
+
+
+        
+    //notas
+    notas=prompt('INGRESE LA NOTA DEL ALUMNO');
+    notas=parseInt(notas);
+    while(notas<-1 || notas>10){
+          notas=prompt('NOTA INVALIDA, INGRESE UNA NOTA ENTRE 0 Y 10');
+
+             
+    }    
+    
+    //SEXO
+    sexo=prompt('INGRESE SEXO CORRESPONDIENTE f/m');
+    while(sexo != "f" && sexo != "m"){
+          sexo=prompt('SEXO INVALIDO, INGRESE EL QUE CORRESPONDE f/m');
+    }
+    
+    
+    //PROMEDIO DE NOTAS
+    notasTotales = notasTotales + notas; // ACUMULADOR DE NOTAS DENTRO DE WHILE Y CALCULOS FUERA DEL WHILE
+    
+    //NOTA MINIMA y sexo de esa persona
+    if(bandera == 0){
+       notaMinima = notas; 
+       bandera = 1;
+    }
+    
+    if(notas<notaMinima){
+       notaMinima = notas;
+       sexoPersona = sexo;     
+    }else{
+        sexoPersona = sexo;
+    }
+    
+    //CANTIDAD DE VARONES Q SU NOTA SEA MAYOR O IGUAL A 6
+    if(sexo == "m" && notas >= 6 ){
+       cantidadVarones = cantidadVarones + 1;
+    }
+
+    /*
+    if(sexo == "f" ){  // TENER EN CUENTA Q ESTO FUNCIONA PARA DARLE UN SEXO A UN NUMERO
+        notas = notas;
+ 
+     }
+    */
+        
+    }
+
+
+    
+    // PROMEDIO NOTAS TOTALES
+    notasTotales = notasTotales/5;
+    
+    alert('PROMEDIO DE NOTAS TOTALES ES ' + notasTotales);
+    
+    
+   
+    alert('La nota mas baja es ' + notaMinima + ' y el sexo de esa persona es ' + sexoPersona);
+    
+    alert('La cantidad de varones que sacaron una nota mayor o igual a 6 son ' + cantidadVarones);
 
 }
